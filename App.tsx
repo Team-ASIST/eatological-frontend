255969344dec657846dbe1c5c9e4f7377dc16be5
Sebastian Schneider <sebastian_f_schneider@web.de>
2022-12-01 14:25:52 +0100

    Setup Navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+
+// All Screens
+function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings!</Text>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function NewPlanScreen({ navigation }) {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>New Plan!</Text>
+            <Button
+                title="Home"
+                onPress={() => navigation.navigate('Current Plan')}
+            />
+        </View>
+    );
+}
+
+function CurrentPlan() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Current Plan!</Text>
+        </View>
+    );
+}
+
+function List() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Shopping List!</Text>
+        </View>
+    );
+}
+
+// Stack Navigator for new Plans
+const NewPlan = createNativeStackNavigator();
+
+function PlanStackScreen() {
+    return (
+        <NewPlan.Navigator>
+            <NewPlan.Screen name="MealQuantity" component={NewPlanScreen} />
+            <NewPlan.Screen name="Leftovers" component={NewPlanScreen} />
+        </NewPlan.Navigator>
+    );
+}
+
+// Bottom Tab Navigator
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator initialRouteName="Current Plan"
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
+
+                        if (route.name === 'New Plan') {
+                            iconName = focused
+                                ? 'add'
+                                : 'add-outline'
+                        } else if (route.name === 'Current Plan') {
+                            iconName = focused
+                                ? 'basket'
+                                : 'basket-outline';
+                        } else if (route.name === 'Shopping List') {
+                            iconName = focused
+                                ? 'ios-list'
+                                : 'ios-list-outline';
+                        } else if (route.name === 'Settings') {
+                            iconName = focused
+                                ? 'settings'
+                                : 'settings-outline';
+                        }
+
+                        // You can return any component that you like here!
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                    tabBarActiveTintColor: 'tomato',
+                    tabBarInactiveTintColor: 'gray',
+                })}
+            >
+                <Tab.Screen
+                    name="New Plan"
+                    component={PlanStackScreen}
+                    options={{ headerShown: false, tabBarStyle: { display: 'none' } }}
+                />
+                <Tab.Screen name="Current Plan" component={CurrentPlan} />
+                <Tab.Screen name="Shopping List" component={List} />
+                <Tab.Screen name="Settings" component={SettingsScreen} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
+}
