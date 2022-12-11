e3c81eebb70e788e61452602cd510b636d7393c9
Sebastian Schneider <sebastian_f_schneider@web.de>
2022-12-11 17:34:23 +0100

    Improved UI responsiveness

--- pages/SwapMealsPage/SwapMealsPage.tsx
+++ pages/SwapMealsPage/SwapMealsPage.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import { createBox, createText } from '@shopify/restyle';
 import { Theme } from '../../utils/theme';
-import { TouchableOpacity, ScrollView, Image, Dimensions, View } from "react-native";
+import { TouchableOpacity, ScrollView, Image, Dimensions } from "react-native";
 import { NavigationScreenProp } from "react-navigation";
 import { SwipeListView } from 'react-native-swipe-list-view';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Text = createText<Theme>();
 const Box = createBox<Theme>();
+
+// TypeScript Setup
 
 class MealData {
   id: number;
@@ -26,59 +30,91 @@ class RecipeData {
     this.id = id;
     this.name = name;
   }
+
+  keyExtractor = (): number => {
+    return this.id;
+  }
 }
 
 export type SwapMealsPageProps = {
   navigation: NavigationScreenProp<any, any>
 };
 
+const wait = (timeout : number) => {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
 // Define Recipe Component here
 
 const Recipe = (recipe: RecipeData) => {
   const height = Dimensions.get('window').height;
   return (
-    <Box height={0.25 * height} alignItems="center" backgroundColor="primaryCardBackground">
+    <Box
+      height={0.25 * height}
+      borderRadius={50}
+      alignItems="center"
+      backgroundColor="primaryCardBackground"
+    >
 
-      <Box flexGrow={1}>
+      <Box flexGrow={1} justifyContent={"center"}>
         <Image source={require('../../assets/favicon.png')} />
       </Box>
 
-      <Text variant={"body"}>{recipe.name}</Text>
+      <Box
+        flexDirection={"row"}
+        paddingBottom={"m"}
+        alignItems={"stretch"}
+      >
+        <Text variant={"body"}>{recipe.name}</Text>
+      </Box>
     </Box>
   );
 };
 
+const getListWithNewRecipe = async (currentList: RecipeData[]): Promise<RecipeData[]> => {
+  //TODO Call Backend for SwipeRight
+  const recipes = [new RecipeData(0, "Kimbap"), new RecipeData(1, "Ramen"), new RecipeData(2, "Teriyaki"), new RecipeData(3, "Tonkatsu"), new RecipeData(4, "Sashimi")]
+  return recipes
+}
 
+const getListWithOldRecipe = async (currentList: RecipeData[]): Promise<RecipeData[]> => {
+  //TODO Call Backend for SwipeLeft
+  const recipes = [new RecipeData(0, "Sushi"), new RecipeData(1, "Ramen"), new RecipeData(2, "Teriyaki"), new RecipeData(3, "Tonkatsu"), new RecipeData(4, "Sashimi")]
+  return recipes
+}
 
 const getInitialPlan = (data: MealData[]): RecipeData[] => {
   const recipes = [new RecipeData(0, "Sushi"), new RecipeData(1, "Ramen"), new RecipeData(2, "Teriyaki"), new RecipeData(3, "Tonkatsu"), new RecipeData(4, "Sashimi")]
   return recipes
 };
 
-const wait = (timeout: number) => {
-  return new Promise(resolve => setTimeout(resolve, timeout));
-}
-
-
-
-
-
 const SwapMealsPage = ({ navigation }: SwapMealsPageProps) => {
+  // Load Initial data for recipes
   const meals = [new MealData(0, 2), new MealData(1, 1)]
-  const recipeList = getInitialPlan(meals)
+  const [recipeList, setRecipeList] = useState(getInitialPlan(meals))
 
   // Handle Swiping Input
-  const [refreshing, setRefreshing] = React.useState(false);
+  const swipeLeft = async (rowKey: any) => {
+    console.log('onLeftAction', rowKey as number);
+    //TODO Load previous recipe at rowKey in recipeList
+    const newList = await getListWithOldRecipe(recipeList)
+    setRecipeList(newList)
 
-  const swipeLeft = React.useCallback(() => {
-    setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false));
-  }, []);
+  };
 
-  const swipeRight = React.useCallback(() => {
-    setRefreshing(true);
-    wait(2000).then(() => setRefreshing(false));
-  }, []);
+  const swipeRight = async (rowKey: any) => {
+    console.log('onRightAction', rowKey as number);
+    //TODO Load new recipe at rowKey in recipeList
+    const newList = await getListWithNewRecipe(recipeList)
+    setRecipeList(newList)
+  };
+
+  const onRowDidOpen = async (rowKey: any, rowMap: any) => {
+    await wait(1000)
+    if (rowMap[rowKey]) {
+      rowMap[rowKey].closeRow();
+    }
+  }
 
   const height = Dimensions.get('window').height;
   const width = Dimensions.get('window').width;
@@ -88,6 +124,21 @@ const SwapMealsPage = ({ navigation }: SwapMealsPageProps) => {
 
       <SwipeListView
         data={recipeList}
+        keyExtractor={(data) => "" + data.id}
+
+        onRowDidOpen={onRowDidOpen}
+
+        stopLeftSwipe={75}
+        stopRightSwipe={-75}
+
+        leftActivationValue={70}
+        rightActivationValue={-70}
+        leftActionValue={70}
+        rightActionValue={-70}
+
+        onLeftAction={swipeLeft}
+        onRightAction={swipeRight}
+
         renderItem={(data, rowMap) => (
           <Box paddingTop={"l"}>
             {
@@ -96,28 +147,44 @@ const SwapMealsPage = ({ navigation }: SwapMealsPageProps) => {
           </Box>
         )}
         renderHiddenItem={(data, rowMap) => (
-          <Box height={0.5 * height} paddingTop={"l"} flexDirection={"row"}>
+          <Box height={0.25 * height} paddingTop={"l"} flexDirection={"row"}>
 
             <Box flex={1}>
-              <TouchableOpacity onPress={() => swipeLeft()}>
-                <Box height={height} alignItems={"flex-start"} paddingLeft={"s"} backgroundColor="mainForeground">
-                  <Text variant={"body"} color={"primaryCardText"}>SwipeLeft</Text>
+              <TouchableOpacity onPress={() => swipeLeft(data.item.id)}>
+                <Box
+                  height={0.25 * height}
+                  borderBottomLeftRadius={50}
+                  borderTopLeftRadius={50}
+                  alignItems={"flex-start"}
+                  justifyContent={"center"}
+                  paddingLeft={"s"}
+                  backgroundColor="mainForeground"
+                >
+                  <Ionicons name="arrow-undo-outline" size={40} color="white" />
                 </Box>
               </TouchableOpacity>
             </Box>
 
             <Box flex={1}>
-              <TouchableOpacity onPress={() => swipeRight()}>
-                <Box height={height} alignItems={"flex-end"} paddingRight={"s"} backgroundColor="secondaryCardBackground">
-                  <Text variant={"body"} color={"primaryCardText"}>SwipeRight</Text>
+              <TouchableOpacity onPress={() => swipeRight(data.item.id)}>
+                <Box
+                  height={0.25 * height}
+                  borderBottomRightRadius={50}
+                  borderTopRightRadius={50}
+                  alignItems={"flex-end"}
+                  justifyContent={"center"}
+                  paddingRight={"s"}
+                  backgroundColor="secondaryCardBackground"
+                >
+                  <Ionicons name="arrow-redo-outline" size={40} color="white" />
                 </Box>
               </TouchableOpacity>
             </Box>
 
           </Box>
         )}
-        leftOpenValue={75}
-        rightOpenValue={-75}
+        leftOpenValue={50}
+        rightOpenValue={-50}
       />
 
     </Box >
