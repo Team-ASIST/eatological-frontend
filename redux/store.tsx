5eb7bb7ba7fe0e3e2f11b1a078f65522e7963f8f
seb711 <sebastian@kosaks.de>
2022-12-28 18:25:09 +0100

    added basic setup for persisting storage

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,13 +1,37 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import newPlanReducer from './slice/newPlanSlice'
 import currentPlanReducer from './slice/currentPlanSlice'
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: AsyncStorage,
+}
+
+const persistedReducer = persistReducer(persistConfig, currentPlanReducer)
 
 export const store = configureStore({
   reducer: {
     newPlan: newPlanReducer,
-    currentPlan: currentPlanReducer
-  }
+    currentPlan: persistedReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
