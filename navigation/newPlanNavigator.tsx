c6d2194930d3eecbc5e230a7a7b2be4e4dce3a75
Sebastian Schneider <65763422+SeppFS@users.noreply.github.com>
2022-12-28 16:15:28 +0100

    35 style react navigation (#58)
    * #35 Outsource Navigation from App.tsx to navigation folder
    
    * #35 Add Theme.ts fonts and change Icon
    
    * Adjust TabNavigator to match SafeAreaView
    
    * Delete .vscode directory

---
+++ navigation/newPlanNavigator.tsx
@@ -0,0 +1,25 @@
+import { createBox } from '@shopify/restyle';
+import theme, { Theme } from '../utils/theme';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList } from './types';
+import { SafeAreaView } from 'react-native';
+import MealQuantityScreen from '../pages/NewPlanPage/MealQuantity';
+import LeftoversScreen from '../pages/NewPlanPage/Leftovers';
+import SwapMealsPage from '../pages/SwapMealsPage/SwapMealsPage';
+
+const NewPlan = createNativeStackNavigator<RootStackParamList>();
+const Box = createBox<Theme>();
+
+export const PlanStackScreen = () => {
+    return (
+      <Box backgroundColor="mainBackground" flex={1}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <NewPlan.Navigator>
+            <NewPlan.Screen name="MealQuantity" options={{ headerShown: false }} component={MealQuantityScreen} />
+            <NewPlan.Screen name="LeftOvers" options={{ headerShown: false }} component={LeftoversScreen} />
+            <NewPlan.Screen name="SwapMeals" options={{ headerShown: false }} component={SwapMealsPage} />
+          </ NewPlan.Navigator>
+        </SafeAreaView>
+      </Box>
+    );
+  }
