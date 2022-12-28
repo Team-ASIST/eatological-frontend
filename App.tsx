1393c614b298982c8ffbe8c7665d1d39035f8e8d
Sebastian Schneider <sebastian_f_schneider@web.de>
2022-12-28 12:57:14 +0100

    #35 Outsource Navigation from App.tsx to navigation folder

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import { useColorScheme, Text, SafeAreaView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBox, ThemeProvider } from '@shopify/restyle';
+import { useColorScheme, Text } from 'react-native';
+import { ThemeProvider } from '@shopify/restyle';
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
 import theme, { darkTheme, Theme } from './utils/theme';
@@ -13,32 +10,7 @@ import {
   Fraunces_500Medium,
   Fraunces_700Bold,
 } from '@expo-google-fonts/fraunces';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import CurrentPlan from './pages/CurrentPlanPage/CurrentPlanPage';
-import GroceryListPage from './pages/GroceryListPage/GroceryListPage';
-import SettingsPage from './pages/SettingsPage/SettingsPage';
-import SwapMealsPage from './pages/SwapMealsPage/SwapMealsPage';
-import { RootStackParamList, RootTabParamList } from './navigation/types';
-import MealQuantityScreen from './pages/NewPlanPage/MealQuantity';
-import LeftoversScreen from './pages/NewPlanPage/Leftovers';
-
-const NewPlan = createNativeStackNavigator<RootStackParamList>();
-const Tab = createBottomTabNavigator<RootTabParamList>();
-const Box = createBox<Theme>();
-
-const PlanStackScreen = () => {
-  return (
-    <Box backgroundColor="mainBackground" flex={1}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <NewPlan.Navigator>
-          <NewPlan.Screen name="MealQuantity" options={{ headerShown: false }} component={MealQuantityScreen} />
-          <NewPlan.Screen name="LeftOvers" options={{ headerShown: false }} component={LeftoversScreen} />
-          <NewPlan.Screen name="SwapMeals" options={{ headerShown: false }} component={SwapMealsPage} />
-        </ NewPlan.Navigator>
-      </SafeAreaView>
-    </Box>
-  );
-}
+import { TabNavigator } from './navigation/tabNavigator';
 
 const App = () => {
   const colorTheme = useColorScheme();
@@ -54,56 +26,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <ThemeProvider theme={colorTheme === 'dark' ? darkTheme : theme}>
-        <NavigationContainer>
-          <Tab.Navigator initialRouteName="CurrentPlan"
-            screenOptions={
-              ({ route }) => ({
-                headerShown: false,
-                tabBarIcon: ({ focused, color, size }) => {
-                  let iconName: string = "";
-
-                  if (route.name === 'NewPlan') {
-                    iconName = focused
-                      ? 'add'
-                      : 'add-outline'
-                  } else if (route.name === 'GroceryList') {
-                    iconName = focused
-                      ? 'basket'
-                      : 'basket-outline';
-                  } else if (route.name === 'CurrentPlan') {
-                    iconName = focused
-                      ? 'ios-list'
-                      : 'ios-list-outline';
-                  } else if (route.name === 'Settings') {
-                    iconName = focused
-                      ? 'settings'
-                      : 'settings-outline';
-                  }
-
-                  return <Ionicons name={iconName} size={size} color={color} />;
-                },
-                // Hide the following routes from the bottom-tabs
-                tabBarButton: [
-                  "NewPlan"
-                ].includes(route.name)
-                  ? () => {
-                    return null;
-                  }
-                  : undefined,
-                tabBarActiveTintColor: theme.colors.primaryButtonColor,
-                tabBarInactiveTintColor: theme.colors.inactiveButtonColor,
-              })}
-          >
-            <Tab.Screen
-              name="NewPlan"
-              component={PlanStackScreen}
-              options={{ headerShown: false, tabBarStyle: { display: 'none' }, unmountOnBlur: true }}
-            />
-            <Tab.Screen name="GroceryList" options={{ headerShown: false }} component={GroceryListPage} />
-            <Tab.Screen name="CurrentPlan" options={{ headerShown: false }} component={CurrentPlan} />
-            <Tab.Screen name="Settings" options={{ headerShown: false }} component={SettingsPage} />
-          </Tab.Navigator>
-        </NavigationContainer>
+        <TabNavigator />
       </ThemeProvider>
     </Provider>
   );
