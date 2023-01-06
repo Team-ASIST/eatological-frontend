bf4ca2a45987f1bbcb75f5bd139db8d4f67b3eac
seb711 <sebastian@kosaks.de>
2023-01-06 23:31:35 +0100

    moved redux related api calls to redux actions

--- redux/slice/currentPlanSlice.tsx
+++ redux/slice/currentPlanSlice.tsx
@@ -1,43 +1,214 @@
-import { createSlice, current, nanoid } from '@reduxjs/toolkit'
-import { Meal, largeGrocery } from '../../utils/dataTypes'
-import { RootState } from '../store'
+import { createAsyncThunk, createSlice, current, nanoid } from '@reduxjs/toolkit'
+import { backend } from '../../utils/axios/config'
+import { Meal, LargeGrocery, Grocery, Ingredient } from '../../utils/dataTypes'
+import { AppDispatch, RootState } from '../store'
 
 
 interface IState {
-    recipes: Meal[],
-    groceries: largeGrocery[]
+    recipes: Meal[], // TODO: Now we have meal and recipe type, this is very confusing
+    groceries: LargeGrocery[],
+    ingredients: Ingredient[]
 }
 
 const initialState: IState = {
     recipes: [],
-    groceries: []
+    groceries: [],
+    ingredients: [],
 }
 
 const currentPlanSlice = createSlice({
     name: 'currentPlan',
     initialState,
     reducers: {
-        updateRecipes(state, action) {
-            const { recipes } = action.payload
-            state.recipes = recipes
+        resetGroceries(state) {
+            state.groceries = [] as LargeGrocery[]
         },
-        updateGroceries(state, action) {
-            const { groceries } = action.payload
-            state.groceries = groceries
-        },
-        buyGroc(state, action) {
-            const { id } = action.payload
-            const largeGrocery = state.groceries.find(grocery => grocery.ingredientId === id)
-            if (largeGrocery) {
-                largeGrocery.grocery.bought = largeGrocery.grocery.required
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(acceptPlan.fulfilled, (state, action) => {
+                const meals = action.payload as Meal[]
+                state.recipes = meals
+            })
+            .addCase(getGroceries.fulfilled, (state, action) => {
+                const groceries = action.payload as Grocery[]
+                let largeGroceries: LargeGrocery[] = state.groceries
+
+                for (const grocery of groceries) {
+                    let largeGrocery = state.groceries.find(largeGrocery => largeGrocery.ingredientId === grocery.ingredientId)
+                    if (largeGrocery) {
+                        if (largeGrocery.grocery.bought < grocery.bought) {
+                            Object.assign(largeGrocery, {
+                                ...largeGrocery,
+                                grocery: {
+                                    ...largeGrocery.grocery,
+                                    bought: largeGrocery.grocery.required
+                                }
+                            });
+                        }
+                    } else {
+                        for (const ing of state.ingredients) {
+                            if (grocery.ingredientId === ing.id) {
+                                largeGroceries.push({
+                                    ingredientId: grocery.ingredientId,
+                                    grocery: grocery,
+                                    ingredient: ing
+                                })
+                            }
+                        }
+                    }
+                }
+
+                state.groceries = largeGroceries
+            })
+            .addCase(getIngredients.fulfilled, (state, action) => {
+                const ingredients = action.payload as Ingredient[]
+
+                state.ingredients = ingredients
+            })
+            .addCase(updateGroceries.fulfilled, (state, action) => {
+                const groceries = action.payload as Grocery[]
+
+                for (const grocery of groceries) {
+                    let largeGrocery = state.groceries.find(largeGrocery => largeGrocery.ingredientId === grocery.ingredientId)
+                    if (largeGrocery) {
+                        // Local missing information from backend
+                        if (largeGrocery.grocery.bought < grocery.bought) {
+                            Object.assign(largeGrocery, {
+                                ...largeGrocery,
+                                grocery: {
+                                    ...largeGrocery.grocery,
+                                    bought: largeGrocery.grocery.required
+                                }
+                            });
+                        }
+                    }
+                }
             }
-        }
+            )
     }
 })
 
-export const { updateRecipes, updateGroceries, buyGroc } = currentPlanSlice.actions
+export const { resetGroceries } = currentPlanSlice.actions
+
+export const updateGroceries = createAsyncThunk<
+    Grocery[],
+    Grocery[],
+    {
+        dispatch: AppDispatch,
+        state: RootState
+    }
+>(
+    'currentPlan/updateGroceries',
+    async (groceries, thunkApi) => {
+        let oldGroceries = thunkApi.getState().currentPlan.groceries
+
+        let updatedGroceries: Grocery[] = oldGroceries.map(obj => groceries.find(o => o.ingredientId === obj.ingredientId) || obj.grocery);
+
+        let groceriesArg = JSON.stringify(updatedGroceries)
+        try {
+            // Get Groceries for the User
+            const response = await backend.put(
+                '/groceries/buy',
+                {},
+                {
+                    headers: {
+                        'Groceries': groceriesArg
+                    }
+                }
+            )
+
+            if (response.status = 200) {
+                // Return data
+
+                return response.data.groceries as Grocery[]
+            }
+            console.error("Call BuyGrocery aborted!")
+        } catch (error) {
+            // Call erroneous
+            console.error(error)
+        }
+        // Send Empty GroceryList
+        return [] as Grocery[]
+    }
+)
+
+export const acceptPlan = createAsyncThunk<
+    Meal[],
+    Meal[]
+>(
+    'currentPlan/acceptPlan',
+    async (meals, thunkApi) => {
+        try {
+            const response = await backend.post(
+                '/plan/accept',
+                {},
+                { headers: {} }
+            )
+            thunkApi.dispatch(resetGroceries())
+            thunkApi.dispatch(getGroceries())
+            return meals
+        } catch (error) {
+            // Call erroneous
+            console.error(error)
+        }
+
+        return [] as Meal[]
+    }
+)
+
+export const getIngredients = createAsyncThunk<
+    Ingredient[]
+>(
+    'currentPlan/getIngredients',
+    async () => {
+        try {
+            const response = await backend.get(
+                '/ingredients'
+            )
+
+            if (response.status = 200) {
+                // Extract data and parse results into Ingredient Array
+                let ingredients: Ingredient[] = response.data
+                return ingredients
+            }
+            console.error("Call Ingredients aborted!")
+
+        } catch (error) {
+            console.error(error)
+        }
+        return [] as Ingredient[]
+    }
+)
+
+export const getGroceries = createAsyncThunk<
+    Grocery[]
+>(
+    'currentPlan/getGroceries',
+    async () => {
+        try {
+            // Get Groceries for the User
+            const response = await backend.get(
+                '/groceries'
+            )
+
+            if (response.status = 200) {
+                // Return data
+                console.log(response.data.groceries)
+                return response.data.groceries as Grocery[]
+            }
+            console.error("Call Groceries aborted!")
+        } catch (error) {
+            // Call erroneous
+            console.error(error)
+        }
+        // Send Empty GroceryList
+        return [] as Grocery[]
+    }
+)
 
 export const selectAllRecipes = (state: RootState) => state.currentPlan.recipes
+export const selectAllIngredients = (state: RootState) => state.currentPlan.ingredients
 export const selectNewPlanConfiguration = (state: RootState) => state.newPlan
 export const selectSortedGroceries = (state: RootState) => {
     return state.currentPlan.groceries.slice().sort(function (grocery1, grocery2) {
@@ -54,7 +225,7 @@ export const selectSortedGroceries = (state: RootState) => {
         } else {
             return 1
         }
-    }) as largeGrocery[]
+    }) as LargeGrocery[]
 }
 
 export default currentPlanSlice.reducer
