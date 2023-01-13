9d1188d4e6589378136e4b970fcde5cbdea4a6b2
Sebastian Schneider <sebastian_f_schneider@web.de>
2023-01-13 20:29:49 +0100

    #86 Fix broken return fork in RecipePage

--- navigation/types.tsx
+++ navigation/types.tsx
@@ -1,16 +1,9 @@
-import { Meal } from "../utils/dataTypes";
-
 export type RootStackParamList = {
     MealQuantity: undefined;
     LeftOvers: undefined;
     FoodPreferences: undefined;
     SwapMeals: undefined;
 };
-
-export type SettingsStackParamList = {
-    User: undefined;
-    Restrictions: undefined;
-}
 
 export type RootTabParamList = {
     NewPlan: undefined;
